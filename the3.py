998a6da7b7e1d20322407d961229a718333cfcac
HasanYldz <hasanyldz70@gmail.com>
2021-01-24 17:40:19 +0300

    stock_check complete

--- the3.py
+++ the3.py
@@ -52,9 +52,6 @@ def calculate_price(part_list):
     return helper(make_tree(part_list))
 
 
-tre = ["bike", (2, ["wheel", (1, ["rim", 60.]), (1, ["spoke", 120.]),(1, ["hub", (2, ["gear", 25.]), (1, ["axle", (5, ["bolt", 0.1]), (7, ["nut", 0.15])])])]), (1, ["frame", (1, ["rearframe", 175.]), (1, ["frontframe", (1, ["fork", 22.5]), (2, ["handle", 10.])])])]
-
-
 def required_parts(part_list):
     def helper(x):
         if type(x[1]) == float:
@@ -65,8 +62,32 @@ def required_parts(part_list):
             for a in range(len(branch)):
                 res.append((x[i][0]*branch[a][0], branch[a][1]))
         return res
+
     return helper(make_tree(part_list))
+
+
+tre = ["bike", (2, ["wheel", (1, ["rim", 60.]), (1, ["spoke", 120.]),(1, ["hub", (2, ["gear", 25.]), (1, ["axle", (5, ["bolt", 0.1]), (7, ["nut", 0.15])])])]), (1, ["frame", (1, ["rearframe", 175.]), (1, ["frontframe", (1, ["fork", 22.5]), (2, ["handle", 10.])])])]
+stock_list = [(2, "rim"), (2, "spoke"), (4, "gear"), (8, "bolt"), (12, "nut"), (1, "rearframe"), (1, "fork"), (1, "handle")]
+
+def stock_check(part_list, stock_list):
+    need = required_parts(part_list)
+    need_catalogue = [need[a][1] for a in range(len(need))]
+    necessary_stock = [stock_list[a] for a in range(len(stock_list)) if stock_list[a][1] in need_catalogue ]
+    stock_catalogue = [necessary_stock[a][1] for a in range(len(necessary_stock))]
+    short = []
+    for part in need:
+        if part[1] in stock_catalogue:
+            ind = stock_catalogue.index(part[1])
+            l = part[0] - stock_list[ind][0]
+            if l > 0:
+                short.append((part[1], l))
+        else:
+            short.append((part[1], part[0]))
+    return short
+
+
 
 # print(make_tree(lst))
 # print(calculate_price(lst))
-print(required_parts(lst))
+# print(required_parts(lst))
+# print(stock_check(lst,stock_list))

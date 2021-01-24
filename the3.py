e37b9ddc197329ec75e4ae017cd72a560aa2c85c
HasanYldz <hasanyldz70@gmail.com>
2021-01-24 17:14:41 +0300

    required_parts complete

--- the3.py
+++ the3.py
@@ -39,8 +39,6 @@ lst =        [["bike", (2, "wheel"), (1, "frame")],
              ["fork", 22.5],
              ["handle", 10.]]
 
-tre =    ["bike", (2, ["wheel", (1, ["rim", 60.]), (1, ["spoke", 120.]),(1, ["hub", (2, ["gear", 25.]), (1, ["axle", (5, ["bolt", 0.1]), (7, ["nut", 0.15])])])]), (1, ["frame", (1, ["rearframe", 175.]), (1, ["frontframe", (1, ["fork", 22.5]), (2, ["handle", 10.])])])]
-deneme = ['bike', (2, ['wheel', (1, ['rim', 60.0]), (1, ['spoke', 120.0]), (1, ['hub', (2, ['gear', 25.0]), (1, ['axle', (5, ['bolt', 0.1]), (7, ['nut', 0.15])])])]), (1, ['frame', (1, ['rearframe', 175.0]), (1, ['frontframe', (1, ['fork', 22.5]), (2, ['handle', 10.0])])])]
 
 
 def calculate_price(part_list):
@@ -53,5 +51,22 @@ def calculate_price(part_list):
         return res
     return helper(make_tree(part_list))
 
+
+tre = ["bike", (2, ["wheel", (1, ["rim", 60.]), (1, ["spoke", 120.]),(1, ["hub", (2, ["gear", 25.]), (1, ["axle", (5, ["bolt", 0.1]), (7, ["nut", 0.15])])])]), (1, ["frame", (1, ["rearframe", 175.]), (1, ["frontframe", (1, ["fork", 22.5]), (2, ["handle", 10.])])])]
+
+
+def required_parts(part_list):
+    def helper(x):
+        if type(x[1]) == float:
+            return [(1, x[0])]
+        res = []
+        for i in range(1, len(x)):
+            branch = helper(x[i][1:][0])
+            for a in range(len(branch)):
+                res.append((x[i][0]*branch[a][0], branch[a][1]))
+        return res
+    return helper(make_tree(part_list))
+
 # print(make_tree(lst))
 # print(calculate_price(lst))
+print(required_parts(lst))
